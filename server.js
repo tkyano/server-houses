214feb8dbeb62e933e2946d7a8fd2ed9cfbf13d6
Timothy Kyano <tdkyano05@gmail.com>
2025-11-06 12:22:39 -0500

    inclass stuff

--- server.js
+++ server.js
@@ -1,9 +1,99 @@
 const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
 const app = express();
 app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./public/images/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+
+let houses = [
+    {
+        "_id":1,
+        "name": "Farmhouse",
+        "size": 2000,
+        "bedrooms": 3,
+        "bathrooms": 2.5,
+        "features": [
+            "wrap around porch",
+            "attached garage"
+        ],
+        "main_image": "farm.webp",
+        "floor_plans": [
+            {
+                "name": "Main Level",
+                "image": "farm-floor1.webp"
+            },
+            {
+                "name": "Basement",
+                "image": "farm-floor2.webp"
+            }
+        ]
+    },
+    {
+        "_id":2,
+        "name": "Mountain House",
+        "size": 1700,
+        "bedrooms": 3,
+        "bathrooms": 2,
+        "features": [
+            "grand porch",
+            "covered deck"
+        ],
+        "main_image": "mountain-house.webp",
+        "floor_plans": [
+            {
+                "name": "Main Level",
+                "image": "mountain-house1.webp"
+            },
+            {
+                "name": "Optional Lower Level",
+                "image": "mountain-house2.webp"
+            },
+            {
+                "name": "Main Level Slab Option",
+                "image": "mountain-house3.jpg"
+            }
+        ]
+    },
+    {
+        "_id":3,
+        "name": "Lake House",
+        "size": 3000,
+        "bedrooms": 4,
+        "bathrooms": 3,
+        "features": [
+            "covered deck",
+            "outdoor kitchen",
+            "pool house"
+        ],
+        "main_image": "farm.webp",
+        "floor_plans": [
+            {
+                "name": "Main Level",
+                "image": "lake-house1.webp"
+            },
+            {
+                "name": "Lower Level",
+                "image": "lake-house2.webp"
+            }
+        ]
+    }
+]
 
 app.get("/api/houses/", (req, res)=>{
     console.log("in get request");
+    res.send(houses);
 });
 
 app.listen(3001), () => {
